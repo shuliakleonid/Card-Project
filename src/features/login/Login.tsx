b0c47c0e76c622fec6fc906a96dc5d67ce367d32
Shuliak Leonid <zeleny777@gmail.com>
2021-05-01 12:02:28 +0300

    add spinner with style

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,12 +1,13 @@
 import React, {ChangeEvent, FC, useState} from 'react';
 import './Login.scss'
+import Spinner from '../../components/spinner/Spinner';
 
 const Login: FC<any> = () => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [validEmail, setValidEmail] = useState<boolean>(false)
   const [validPassword, setValidPassword] = useState<boolean>(false)
-
+  const isLoading = true
 
   const changeEmail = ({target}: ChangeEvent<HTMLInputElement>) => {
     setValidEmail(false)
@@ -31,22 +32,26 @@ const Login: FC<any> = () => {
       <div className="container">
         <div className="box">
           <h2>Login</h2>
-          <form action="">
-            <div className="inputBox">
-              <input type="text" required={true} value={email} onChange={changeEmail}/>
-              <label>Email</label>
-            </div>
-            <div className="inputBox">
-              <input type="password" required={true} value={password} onChange={changePassword}/>
-              <label>Password</label>
-            </div>
-            {validEmail && <p className="error">Enter valid email</p>}
-            {validPassword && <p className="error">Enter longer password more then 6 symbols</p>}
-            <input type="submit" value="Submit" onClick={onSubmit}/>
-          </form>
+          {isLoading
+              ? (<Spinner/>)
+              : (<form action="">
+                <div className="inputBox">
+                  <input type="text" required={true} value={email} onChange={changeEmail}/>
+                  <label>Email</label>
+                </div>
+                <div className="inputBox">
+                  <input type="password" required={true} value={password} onChange={changePassword}/>
+                  <label>Password</label>
+                </div>
+                {validEmail && <p className="error">Enter valid email</p>}
+                {validPassword && <p className="error">Enter longer password more then 6 symbols</p>}
+                <input type="submit" value="Submit" onClick={onSubmit}/>
+              </form>)
+          }
         </div>
       </div>
   )
+
 };
 
 export default Login;
